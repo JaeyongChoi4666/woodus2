0029371c2dda4e4e97f6ab45edce94a49e608197
JaeyongChoi4666 <81898000+JaeyongChoi4666@users.noreply.github.com>
2024-04-06 08:24:33 +0900

    firstInit

---
+++ src/main/java/com/example/woodus2/Woodus2Application.java
@@ -0,0 +1,13 @@
+package com.example.woodus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Woodus2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Woodus2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/woodus2/Woodus2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woodus2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Woodus2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
