23b1fe3d4705c46ff4e7c945e3ef5d7e88d04c08
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2025-02-16 15:51:18 +0900

    editor init 추가가

--- src/main/webapp/resources/assets/js/pages/form-editor.init.js
+++ src/main/webapp/resources/assets/js/pages/form-editor.init.js
@@ -1,1 +1,6 @@
-ClassicEditor.create(document.querySelector("#ckeditor-classic")).then(function(e){e.ui.view.editable.element.style.height="200px"}).catch(function(e){console.error(e)});
+ClassicEditor.create(document.querySelector("#ckeditor-classic"))
+    .then(editor => {
+        window.editor = editor})
+    .catch(function (e) {
+        console.error(e);
+    });
